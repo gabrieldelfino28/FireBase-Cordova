6f1112f2098ae5615291631cae7f744d9f79e174
gabrieldelfino28 <gabrieldelfino28@gmail.com>
2019-11-18 20:20:50 -0300

    FireBase

---
+++ www/firebaseConfig.js
@@ -0,0 +1,12 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyBF6mr_hdGlIhx7_szxD9V-5oDrKsIPETY",
+    authDomain: "aula-cordova.firebaseapp.com",
+    databaseURL: "https://aula-cordova.firebaseio.com",
+    projectId: "aula-cordova",
+    storageBucket: "aula-cordova.appspot.com",
+    messagingSenderId: "334872176453",
+    appId: "1:334872176453:web:cc732d50176dc0041ac02c",
+    measurementId: "G-05KFK3HLVW"
+  };
+
+  
